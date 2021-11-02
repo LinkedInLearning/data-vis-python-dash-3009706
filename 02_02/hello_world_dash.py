dc3e71a65d88ca0265dfbfe849308e54acd12bc7
unknown <prandrews@hotmail.co.uk>
2021-11-02 10:16:24 +0000

    Recorded sucessfully.

--- 02_02/hello_world_dash.py
+++ 02_02/hello_world_dash.py
@@ -3,7 +3,7 @@ from dash import html
 
 app = dash.Dash(__name__)
 
-app.layout = html.P("Hello World!")
+app.layout = html.P("Hello Dash!")
 
 if __name__ == "__main__":
     app.run_server(debug=True)
