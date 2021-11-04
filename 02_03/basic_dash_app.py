cbfddcabc4a5232cf5b570446e9f8d1255faff95
unknown <prandrews@hotmail.co.uk>
2021-11-04 09:16:08 +0000

    Recorded succesfully.

--- 02_03/basic_dash_app.py
+++ 02_03/basic_dash_app.py
@@ -1,9 +1,26 @@
 import dash
 from dash import html
+from dash import dcc
+import plotly.express as px
+import pandas as pd
+
+# Read in data
+data = pd.read_csv("precious_metals_prices_2018_2021.csv", usecols=["DateTime", "Gold"])
+
+# Create a plotly figure for use by dcc.Graph()
+fig = px.line(data, x="DateTime", y="Gold", title="Precious Metal Prices 2018-2021")
 
 app = dash.Dash(__name__)
+app.title = "Precious Metal Prices 2018-2021"
 
-app.layout = html.P("Hello Dash!")
+app.layout = html.Div(
+    id="app-container",
+    children=[
+        html.H1("Precious Metal Prices 2018-2021"),
+        html.P("Results in USD/oz"),
+        dcc.Graph(figure=fig)
+    ]
+)
 
 if __name__ == "__main__":
     app.run_server(debug=True)
